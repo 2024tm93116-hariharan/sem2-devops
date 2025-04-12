b455e83e22522058ec805c740fb736ff1d2e85df
2024tm93116-hariharan <2024tm93116@wilp.bits-plani.ac.in>
2025-04-13 01:06:11 +0530

    changing the loading message

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send("Hello, World! Introduction to devops");
+  res.send("Hello, World! Introduction to devops 2025");
 });
 
 app.listen(port, () => {
